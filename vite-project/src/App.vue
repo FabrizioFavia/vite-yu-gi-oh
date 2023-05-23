c7c5e6575690704547fa6a49bc3dd4a812582380
FabrizioFavia <personal.favia@gmail.com>
2023-05-23 14:45:20 +0200

    feat: created project.

---
+++ vite-project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
